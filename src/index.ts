83aab7f6219d446102c5c79df05828d628ac4f00
Herman J. Radtke III <herman@hermanradtke.com>
2020-08-12 08:16:14 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import {
+  GET_MANY,
+  GET_LIST,
+  CREATE,
+  UPDATE,
+  UPDATE_MANY,
+  GET_ONE,
+  GET_MANY_REFERENCE,
+  DELETE_MANY,
+  DELETE,
+} from 'ra-core'
+import type {
+  GetManyReferenceParams,
+  UpdateManyParams,
+  CreateParams,
+  DeleteManyParams,
+  GetListParams,
+  GetManyParams,
+  GetOneParams,
+  DeleteParams,
+  ListParams,
+  UpdateParams,
+  Identifier,
+} from 'ra-core'
+
+console.log('here');
